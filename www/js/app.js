47edc689db9649605b93548058a7201a66e6cc3e
B Paul Anderson <benpaul.anderson@gmail.com>
2016-04-05 22:56:19 -0500

    Add eventsList service and eventView

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,7 @@
 angular.module('concert-search', ['ionic', 'ngSanitize'])
 
+.value('APPID', 'concert-search')
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.StatusBar) {
