52202da06064c0ed7480b7cef041461ef8bff7c8
B Paul Anderson <benpaul.anderson@gmail.com>
2016-04-12 22:13:21 -0500

    Add debounce service

--- www/js/services.js
+++ www/js/services.js
@@ -362,7 +362,33 @@ angular.module('concert-search')
       }
     };
   };
-}]);
+}])
+
+.factory('debounce', function () {
+  // Credit: https://davidwalsh.name/javascript-debounce-function
+  //
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+  //
+  // Default wait is 100ms.
+  return function debounce(func, wait, immediate) {
+    isNaN(wait) && (wait = 100);
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+});
 
 var processEvent = function (rawEvent) {
   rawEvent.latLng = {
